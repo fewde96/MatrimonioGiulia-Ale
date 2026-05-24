6b5bd474b9c8e272f6555fecbf6deabdd5caed4b
Federico Paolucci <Federico.Paolucci-external@generali-invest.com>
2026-05-24 10:01:15 +0200

    aggiungi config.js per GitHub Pages

---
+++ config.js
@@ -0,0 +1,3 @@
+// NON committare questo file! È escluso da .gitignore
+const SUPABASE_URL = 'https://nrndknzyakrkwvhgledt.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ybmRrbnp5YWtya3d2aGdsZWR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1NzMzNTIsImV4cCI6MjA5NTE0OTM1Mn0.H1W1fswjQmFYUlgJfuhfdEqc59fJYPo3q3OJ9cm-wLo';
